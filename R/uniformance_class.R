3d6157ca8b2b8092b85cd93fb3467db5481a9529
jheyday <jackheywoodday@gmail.com>
2023-10-03 12:17:21 +1100

    Implemented Startime, Endtime, SampleFrequency

--- R/uniformance_class.R
+++ R/uniformance_class.R
@@ -1,5 +1,5 @@
 library(processx)
-library(XML)
+library(xml2)
 library(R6)
 
 #' Uniformance Class
@@ -18,15 +18,16 @@ library(R6)
 #' @examples
 #' example <- Uniformance$new("MALSHW1")
 #' @import R6
-#' @import XML
+#' @import xml2
 #' @import processx
 #' @export 
 Uniformance <- R6Class("Uniformance",
 private = list(
   m_Starttime =NULL,
   m_Endtime = NULL,
-  m_tags = list(),
-  m_phdexe = NULL
+  m_phdexe = NULL,
+  m_samplefrequency = NULL,
+  m_tags = list()
   
 ),
 public = list(
@@ -38,63 +39,157 @@ public = list(
   #' @description
   #' Initialises instances of the uniformance class
   #' 
-  initialize = function(hostname = NA, UserName='', Password='', Port=3000, Starttime="NOW-1D", Endtime="NOW"){
+  initialize = function(hostname = NA, Username='', Password='', Port=3000, starttime='NOW-1D', m_Endtime='NOW', m_samplefrequency=0){
     
-    self$Hostname <- hostname
-    self$Username <- UserName
-    self$Password <- Password
-    self$Port <- Port
-    self$m_Starttime <- Starttime
-    self$m_Endtime <- Endtime
-    package_location <- gsub("/","//",system.file(package = "UniformanceGrabber"))
-    self$m_phdexe = paste(package_location, "//bin//phdapinetinterface.exe",sep="")
+    self$Hostname             <- hostname
+    self$Username             <- Username
+    self$Password             <- Password
+    self$Port                 <- Port
+    private$m_Starttime       <- starttime
+    private$m_Endtime         <- m_Endtime
+    private$m_samplefrequency <- m_samplefrequency
+    package_location <- gsub("/","//",system.file(package = 'UniformanceGrabber'))
+    private$m_phdexe <- paste(package_location, '//bin//phdapinetinterface.exe',sep="")
     
   },
+  
+  ##############################################################################
+  # Parameters Functions
+  ##############################################################################
+  
   #' @description
   #' Checks if tag exists on server and adds to a list of tags that will be grabbed by get_data
   #' @param tag_name 'A.RL_AI7361.BATCH'
-
   add_tag = function(tag_name){
-    
-    tagcheck <- run(self$m_phdexe, c("checktag",
-                                              paste("-h", self$Hostname, sep=""),
-                                              paste("-P", self$Port, sep=""),
-                                              paste("-u", self$Username, sep=""),
-                                              paste("-p", self$Password, sep=""),
-                                              paste("-t", tag_name, sep="")
-    ))
-    if (grepl("found\r\n$", tagcheck)) {
-      self$m_tags <- append(self$m_tags, tag_name)
+    commands <- c("checktag",
+                  paste("-h", self$Hostname, sep=""),
+                  paste("-P", self$Port, sep=""),
+                  paste("-u", self$Username, sep=""),
+                  paste("-p", self$Password, sep=""),
+                  paste("-t", tag_name, sep="")
+                  )
+    tagcheck <- run(private$m_phdexe, commands)
+    #tagcheck <- run(exe, commands)
+    if (endsWith(tagcheck$stdout, "found\r\n")) {
+      private$m_tags <- append(private$m_tags, tag_name)
       print(paste(tag_name, "added to taglist"))
-      
-    } 
-    else if (grepl("system\r\n$", tagcheck)) {
+      return(0)
+    } else if (endsWith(tagcheck$stdout, "system\r\n")) {
       print("Tagname was not found, check tagname and try again")
-    } 
-    else {
+      return(1)
+    } else {
       print("Connection to PHD server failed. Check server details")
+      return(1)
     }
   },
+  
   #' @description
-  #' Returns a dataframe of all data using currently set parameters and tags
-  #' This method requires that tags have been added via add_tag()
+  #' Sets the starttime to be passed to the results function 
+  #' @param starttime 'DD/MM/YYYY HH:mm:ss' format
+  set_startime = function(starttime){
+    private$m_Starttime <- starttime
+  },
+  #' @description
+  #' Sets the Endtime to be passed to the results function 
+  #' @param endtime 'DD/MM/YYYY HH:mm:ss' format
+  set_endtime = function(endtime){
+    private$m_Endtime <- endtime
+  },
+  #' @description
+  #' Sets the sample Frequency, value is in seconds 
+  #' @param endtime Default value is 0
+  set_samplefrequency = function(samplefrequency){
+    private$m_samplefrequency <- samplefrequency
+  },
+  
+  #' @description
+  #' See current Starttime 
+  #' @param starttime
+  startime = function(){
+    return(private$m_Starttime)
+  },
+  #' @description
+  #' See current Endtime 
+  #' @param endtime 
+  endtime = function(){
+    return(private$m_Endtime)
+  },
+  #' @description
+  #' See current Sample Frequency 
+  #' @param endtime
+  samplefrequency = function(samplefrequency){
+    return(private$m_samplefrequency)
+  },
+  
+  ##############################################################################
+  # Result Functions
+  ##############################################################################
+  
+  
+  #' @description
+  #' Returns a list of dataframes with current tags and parameters. 
+  #' 
+  #' This method requires that tags have been added via add_tag().
   get_results = function(){
-    if (length(tag) == 0) {
+    if (length(private$m_tags) == 0) {
       stop("Tag list is empty")
     }
-    for (element in tag) {
-      xmloutput <- capture.output(r1 <- run(self$m_phdexe, c("getdata",
-                                                      paste("-h", self$Hostname, sep=""),
-                                                      paste("-P", self$Port, sep=""),
-                                                      paste("-u", self$Username, sep=""),
-                                                      paste("-p", self$Password, sep=""),
-                                                      paste("-t", element, sep=""),
-                                                      paste("-s", self$m_Starttime, sep=""),
-                                                      paste("-e", self$m_Endtime, sep="")
-                                                      )))
-      dataframe.append(XML::xmlToDataFrame((xml <- XML::xmlParse(xmloutput))))
+    dataframe <- list()
+    for (element in private$m_tags) {
+      #xmloutput <- run(exe, c("getdata",
+      #                                                       paste("-h", self$Hostname, sep=""),
+      #                                                       paste("-P", self$Port, sep=""),
+      #                                                       paste("-u", self$Username, sep=""),
+      #                                                       paste("-p", self$Password, sep=""),
+      #                                                       paste("-t", element, sep=""),
+      #                                                       paste("-s", private$m_Starttime, sep=""),
+      #                                                       paste("-e", private$m_Endtime, sep=""),
+      #                                                       paste("-f", private$m_samplefrequency, sep="")
+      #))
+      
+      
+      xmloutput <- run(self$m_phdexe, c("getdata",
+                                       paste("-h", self$Hostname, sep=""),
+                                       paste("-P", self$Port, sep=""),
+                                       paste("-p", self$Password, sep=""),
+                                       paste("-t", element, sep=""),
+                                       paste("-u", self$Username, sep=""),
+                                       paste("-s", private$m_Starttime, sep=""),
+                                       paste("-e", private$m_Endtime, sep=""),
+                                       paste("-f", private$m_samplefrequency, sep="")
+                                       ))
+      
+      rawdata   <-xml2::read_xml(xmloutput$stdout)
+      tagname   <-c(xml_text(xml_find_all(rawdata, xpath = "//TagName")))
+      timestamp <-c(xml_text(xml_find_all(rawdata, xpath = "//TimeStamp")))
+      value     <-c(xml_text(xml_find_all(rawdata, xpath = "//Value")))
+      confidence<-c(xml_text(xml_find_all(rawdata, xpath = "//Confidence")))
+      hostname  <-c(xml_text(xml_find_all(rawdata, xpath = "//HostName")))
+      units     <-c(xml_text(xml_find_all(rawdata, xpath = "//Units")))
+      
+      listoflists = list(tagname    = tagname,
+                         Timestamp  = timestamp,
+                         Value      = value,
+                         Confidence = confidence,
+                         Hostname   = hostname)
+      
+      df <- as.data.frame(do.call(cbind, listoflists))   
+      dataframe <- append(dataframe, list(df))
     }
     return(dataframe)
   }
 )
 )
+
+
+#u <- Uniformance$new('MALSHW1')
+#u$add_tag('A.RL_AI7361.BATCH')
+#u$add_tag('A.RL_AI7361.GRADE')
+#u$set_startime('NOW-3W')
+#u$set_endtime('NOW-1D')
+#u$startime()
+#u$samplefrequency()
+#sand <- u$get_results()
+#print(sand)
+#sand
+
